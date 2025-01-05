405463e31624e982985e441c4eaacb4cd3b11d30
Ilaria44 <91695386+Ilaria44@users.noreply.github.com>
2025-01-05 18:31:07 +0100

    get "=" e "." buttons working correctly

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //variables
-
 let firstNum = 0;
 let secondNum = 0;
 let operation;
+let isSecondNumStart = false;
+let isNumListenerOn = true;
 
 let buttons = document.querySelector(".button-container");
 let display = document.querySelector("#display-content");
 
 //basic math functions
-
 function add(a, b) {
   return a + b;
 }
@@ -26,7 +26,7 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
-  let result = 0;
+  let result;
 
   switch (operator) {
     case "+":
@@ -39,31 +39,32 @@ function operate(a, b, operator) {
 
     case "x":
       result = multiply(a, b);
+      break;
 
     case ":":
       result = divide(a, b);
+      break;
   }
 
   return result;
 }
 
-//show user selection in the display
 
-// function displayNumber(numArray) {
-//   let num = numArray.join("");
-//   display.textContent = num; 
-//   return num;
-// }
-
-
+//displays number, if 0 it replaces it - if already contains a . don't add it
 function displayNumber(num) {
-  if(display.textContent == 0) {
+  if(num === "." && display.textContent.includes(".")) {
+    return;
+  } 
+
+  if(display.textContent == "0" || isSecondNumStart === true) {
     display.textContent = num;
+    isSecondNumStart = false;
 
   } else {
     display.textContent += num;
   }
 }
+
 
 function getUserInput() {
 
@@ -71,21 +72,50 @@ function getUserInput() {
     if(e.target.classList.contains("number")) {
       let number = e.target.textContent;
       displayNumber(number);
-      
     }
 
   }
+
+  
 
   function getOperator(e) {
     if(e.target.classList.contains("operator")) {
       firstNum = Number(display.textContent);
       operation = e.target.textContent;
-      display.textContent = 0;
-
+      isSecondNumStart = true;
+  
       buttons.removeEventListener("click", getOperator);
+      buttons.addEventListener("click", getResult);
 
+      if(!isNumListenerOn) {
+        buttons.addEventListener("click", getNum); 
+      }
       
     }
+
+  }
+
+
+
+  function getResult(e) {
+    if(e.target.id === "operate-button") {
+      secondNum = Number(display.textContent);
+      buttons.removeEventListener("click", getNum);
+      isNumListenerOn = false;
+
+      let result = operate(firstNum, secondNum, operation);
+    
+      display.textContent = result;
+
+      firstNum = 0;
+      secondNum = 0;
+
+
+      buttons.removeEventListener("click", getResult);
+      buttons.addEventListener("click", getOperator);
+      
+    }
+
 
   }
  

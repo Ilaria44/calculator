360bf5f48baa7e29785b7f0f7a130c16efa5f09f
Ilaria44 <91695386+Ilaria44@users.noreply.github.com>
2025-01-05 12:43:34 +0100

    get user input and display functionality

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//variables
+
+let firstNum = 0;
+let secondNum = 0;
+let operation;
+
+let buttons = document.querySelector(".button-container");
+let display = document.querySelector("#display-content");
+
+//basic math functions
+
 function add(a, b) {
   return a + b;
 }
@@ -13,11 +24,6 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
-
-
-let firstNum = 0;
-let secondNum = 0;
-let operation;
 
 function operate(a, b, operator) {
   let result = 0;
@@ -40,3 +46,56 @@ function operate(a, b, operator) {
 
   return result;
 }
+
+//show user selection in the display
+
+// function displayNumber(numArray) {
+//   let num = numArray.join("");
+//   display.textContent = num; 
+//   return num;
+// }
+
+
+function displayNumber(num) {
+  if(display.textContent == 0) {
+    display.textContent = num;
+
+  } else {
+    display.textContent += num;
+  }
+}
+
+function getUserInput() {
+
+  function getNum(e) {
+    if(e.target.classList.contains("number")) {
+      let number = e.target.textContent;
+      displayNumber(number);
+      
+    }
+
+  }
+
+  function getOperator(e) {
+    if(e.target.classList.contains("operator")) {
+      firstNum = Number(display.textContent);
+      operation = e.target.textContent;
+      display.textContent = 0;
+
+      buttons.removeEventListener("click", getOperator);
+
+      
+    }
+
+  }
+ 
+  buttons.addEventListener("click", getNum);
+  buttons.addEventListener("click", getOperator)
+
+  
+  
+}  
+
+
+getUserInput();
+
